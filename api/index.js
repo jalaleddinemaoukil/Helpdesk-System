bb0ec235ae70b8c189bed9c56d4c7d2bb551550d
vynejoestar <vynejoestar@gmail.com>
2024-05-15 23:42:56 +0100

    Handling Error and Middleware function

---
+++ api/index.js
@@ -0,0 +1,37 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import userRoutes from './routes/user.route.js';
+import authRoutes from './routes/auth.route.js';
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL).then(() => {
+    console.log('Connected to MongoDB')
+}).catch((err) => {
+    console.log(err)
+})
+
+const app = express();
+
+app.use(express.json())
+
+app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
+
+
+app.listen(3000, () => {
+    console.log("Server listening on port 3000")
+});
+
+
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || "Internal Server Error";
+    return res.status(statusCode).json({ 
+        success: false,
+        message,
+        statusCode,
+    });
+})
+
